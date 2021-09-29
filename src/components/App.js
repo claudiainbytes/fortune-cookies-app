712219e8f4469b4599675ec97801269c113b48b8
Claudia Estupinan <claudiaeforero@hotmail.com>
2021-09-29 17:45:05 -0500

    Customizing Fortune Cookie Theme SASS

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.scss';
-import { Dots }  from 'react-preloaders2';
+// import { Dots }  from 'react-preloaders2';
 import { connect } from 'react-redux';
 import { fetchGetQuotes } from '../redux/reducers';
 
@@ -32,16 +32,30 @@ class Presentational extends React.Component {
       <div> 
         <div className="container">
           <div className="row">
-            <div className="column">
-             <h2>Fortune cookie</h2>
-              <br />
-              <p>{ answer.text }<br/> - { (answer.author !== null) ? answer.author : "Anonymous" }</p>
-              <br />
-              <button onClick={this.ask}>Break the cookie!</button>
-           </div>
+            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+              <div className="align-xs-left py-25">
+                <h2 className="m-0">Fortune cookie</h2>
+              </div> 
+            </div>
+          </div>  
+          <div className="row">
+            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+              <div className="align-xs-left">
+                  <blockquote className="m-0 quote">
+                    <p>{ answer.text }<br/><small>- { (answer.author !== null) ? answer.author : "Anonymous" }</small></p>              
+                  </blockquote> 
+              </div>   
+            </div>
+          </div>  
+          <div className="row">
+            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+               <div className="align-xs-center py-25">
+                 <button className="m-0" onClick={this.ask}>Break the cookie!</button>
+               </div>
+            </div>
           </div>
         </div>
-        <Dots color={'#f7f7f7'} background="linear-gradient(90deg, rgba(243, 72, 104,1) 20.260775120440467%,rgba(242, 71, 104,1) 20.260775120440467%,rgba(158, 0, 236,1) 80.26163540949759%)" animation="fade" />
+        {/* <Dots color={'#f7f7f7'} background="linear-gradient(90deg, rgba(243, 72, 104,1) 20.260775120440467%,rgba(242, 71, 104,1) 20.260775120440467%,rgba(158, 0, 236,1) 80.26163540949759%)" animation="fade" /> */}
       </div> 
     );
   }
