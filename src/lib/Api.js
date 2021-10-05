dc8eb3791d82ecbafc0eda50b8dbff7e0d7620e5
Claudia Estupinan <claudiaeforero@hotmail.com>
2021-10-04 22:22:54 -0500

    Updates

--- src/lib/Api.js
+++
@@ -1,9 +0,0 @@
-//variable url del servidor
-const BASEURL = "https://type.fit/api";
-
-export const apiGetQuotes = () => {
-    const url = BASEURL + "/quotes" ;
-    return fetch(url)
-        .then(response => response.json())
-        .catch(error => console.error(error))
-};
