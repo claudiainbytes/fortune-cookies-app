f695c10d8aaf79871133bb7770ff3de36d9f5fea
Claudia Estupinan <claudiaeforero@hotmail.com>
2021-10-04 22:13:07 -0500

    Centering the app container using flexbox

--- src/components/App.js
+++ src/components/App.js
@@ -31,39 +31,43 @@ class Presentational extends React.Component {
     answer.author = (answer.author !== null) ? answer.author : "Unknown"; 
     return (
       <div> 
-        <div className="container">
-          <div className="row">
-            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
-              <div className="align-xs-center py-25">
-                <h2 className="m-0">The Fortune Cookie App</h2>
-              </div> 
-            </div>
-          </div>  
-          <div className="row">
-            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
-              <div className="align-xs-left">
-                  <blockquote className="m-0 quote">
-                    <p className="m-0 text-center">"{ answer.text }"</p>
-                    <footer className="mt-10 text-center text-lg-right"><p>- <small>{ answer.author }</small></p></footer> 
-                    <div className="m-0 align-xs-center align-lg-left">
-                      <a id="tweet-quote" className="button button-transparent icon icon-twitter button-icon" href={'https://twitter.com/intent/tweet?text="' + answer.text + '" - ' + answer.author + '&hashtags=FortuneCookie'}></a>             
-                    </div>  
-                  </blockquote> 
-              </div>   
-            </div>
-          </div>  
-          <div className="row">
-            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
-               <div className="align-xs-center py-25">
-                 <button className="button button-lg button-orange m-0 p-10" onClick={this.ask}>Crack the cookie!</button>
-               </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
-               <div className="align-xs-center py-5">
-                 <p className="m-0 text-center credits">by <a href="https://claudiainbytes.github.io/" target="_blank">@claudiainbytes</a></p>
-               </div>
+        <div className="center-center-container">
+          <div className="center-center-child">
+            <div className="container">
+              <div className="row">
+                <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+                  <div className="align-xs-center py-25">
+                    <h2 className="m-0">The Fortune Cookie App</h2>
+                  </div> 
+                </div>
+              </div>  
+              <div className="row">
+                <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+                  <div className="align-xs-left">
+                      <blockquote className="m-0 quote">
+                        <p className="m-0 text-center">"{ answer.text }"</p>
+                        <footer className="mt-10 text-center text-lg-right"><p>- <small>{ answer.author }</small></p></footer> 
+                        <div className="m-0 align-xs-center align-lg-left">
+                          <a id="tweet-quote" className="button button-transparent icon icon-twitter button-icon" href={'https://twitter.com/intent/tweet?text="' + answer.text + '" - ' + answer.author + '&hashtags=FortuneCookie'}></a>             
+                        </div>  
+                      </blockquote> 
+                  </div>   
+                </div>
+              </div>  
+              <div className="row">
+                <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+                  <div className="align-xs-center py-25">
+                    <button className="button button-lg button-orange m-0 p-10" onClick={this.ask}>Crack the cookie!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="column column-xs-100 column-xs-offset-0 column-md-50 column-md-offset-25">
+                  <div className="align-xs-center py-5">
+                    <p className="m-0 text-center credits">by <a href="https://claudiainbytes.github.io/" target="_blank" rel="noreferrer">@claudiainbytes</a></p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
